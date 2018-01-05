8bdb408cf2aef33580228cf9a9577aaa3fa2eb66
websi96 <thomas.weber96@gmail.com>
2018-01-05 11:13:29 +0100

    Database Config

--- model/config.php
+++ model/config.php
@@ -1,6 +1,5 @@
 <?php
 
-$dbconn = pg_connect("host=localhost dbname=h1553755 user=h1553755 password=h1553755");
-
+$dbconn = pg_connect("host=localhost dbname=overwork_9850 user=@55228f0b-3ed9-4894-b108-8f54571780ff.overwork-9850.postgresql.dbs.scalingo.com password=31542/overwork_9850");
 
 ?>
